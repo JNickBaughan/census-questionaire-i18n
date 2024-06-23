1942d0879f7d2c0128dcb108f0d447810080be26
J Nick Baughan <JNickBaughan@gmail.com>
2024-06-23 11:40:23 -0400

    missed one file

--- database/seed/questionToAnswerOptions.seedScript.sql
+++ database/seed/questionToAnswerOptions.seedScript.sql
@@ -1,11 +1,10 @@
 MERGE INTO [dbo].[QuestionToAnswerOptions] AS TARGET
 USING (VALUES
-    (1
-    ,'How many people were living or staying in this house, apartment, or mobile home on April 1, 2020?'
-    ,1,1)
-    ,(2
-    ,'Were there any additional people staying here on April 1, 2020 that you did not include in Question 1?'
-    ,2,2)
+     (1,2,1)
+    ,(2,2,2)
+    ,(3,2,3)
+    ,(4,2,4)
+    ,(5,2,5)
 )
 AS SOURCE ([answerOptionId],[questionId],[order])
 ON TARGET.[questionId] = Source.[questionId] 
